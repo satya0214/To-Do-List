ba19b077b690e07f5ef04f8540e3048348b32bd6
satya0214 <109071698+satya0214@users.noreply.github.com>
2024-03-18 17:02:30 +0530

    To-Do List By Using React

--- src/App.js
+++ src/App.js
@@ -2,56 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import  {useState}  from 'react';
 import ToDoItems from './components/ToDoItems';
-// import Login from './components/Login';
-
-
-// import Heading from './Heading';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-// import Note from './components/Note';
-// import Node from './constants/Constants';
-//  import Card from './components/Card';
-//  import Contact from './components/Contact';
-// import Entry from './components/Entry';
-// import emojiPedia from './components/emojiPedia';
-
- 
-// function createCard(Contact){
-//   return(
-//     <Card 
-//     key ={Contact.id}
-//     name = {Contact.name}
-//     img ={Contact.imgURL}
-//     phone ={Contact.phone}
-//     email={Contact.email}
-//     />
-//   )
-// }
-
-// function createEmoji(emojiPedia){
-//   return(
-//     <Entry 
-//     key={emojiPedia.id}
-//     emoji = {emojiPedia.emoji}
-//     name={emojiPedia.name}
-//     meaning = {emojiPedia.meaning}
-//     />
-//   )
-// }
-
-// function Createnode(node){
-//   return (
-//     <Note 
-//   key = {node.key}
-//   title={node.title}
-//   content = {node.content}
-//   />
-//   )
-// }
- 
-// This is the Loginpage variavle
-// var userIsRegistered = false;
-
 
 function App() {
   const [inputText , setinputText] = useState("");
@@ -113,29 +63,6 @@ function App() {
         </ul>
       </div>
     
-
-
-
-      {/* <Heading /> */}
-      {/* <Header />
-      {Node.map(Createnode)}
-      <Footer /> */}
-
-
-      {/* <Card /> */}
-   
-    {/* <h1 className="heading">My Contacts</h1>
-    {Contact.map(createCard)} */}
-    {/* <h1 className="heading">emojipedia</h1>
-    {emojiPedia.map(createEmoji)} */}
-      {/* <h1>
-        <span className='heading'>emojiPedia</span>
-      </h1>
-      <dl className="meaning">{emojiPedia.map(createEmoji)}</dl> */}
-
-      {/* <Login isRegistered={userIsRegistered}/> */}
-      
-     
       
        </div>
   );
